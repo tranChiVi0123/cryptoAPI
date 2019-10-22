605fa0bdcd3794cfdfda849487a4ae09e20a3d88
tranChiVi0123 <idiots1230@gmail.com>
2019-10-22 18:19:18 +0700

    auth for Post

--- routes/post.js
+++ routes/post.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const postController = require('../app/controllers/postControllers');
 
 
-router.post('/',postController.new);
+router.post('/', postController.new);
 
-router.get('/',postController.view);
+router.get('/', postController.view);
 
-router.put('/:id',postController.update);
+router.put('/:id', auth, postController.update);
 
-router.delete('/:id',postController.delete);
+router.delete('/:id', auth, postController.delete);
 
 module.exports = router;
